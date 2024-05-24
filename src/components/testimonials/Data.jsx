220aec782120f8e7676c9beb40ecff0c58a972ed
betsky2112 <roberthussebastians@gmail.com>
2024-05-24 13:01:14 +0700

    Add testimonial component

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,29 @@
+import Image1 from '../../assets/testimonial1.png'
+import Image2 from '../../assets/testimonial2.png'
+import Image3 from '../../assets/testimonial3.png'
+
+const Data = [
+	{
+		id: 1,
+		image: Image1,
+		title: 'Jhon Doe',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.',
+	},
+	{
+		id: 2,
+		image: Image2,
+		title: 'Harry Clinton',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.',
+	},
+	{
+		id: 3,
+		image: Image3,
+		title: 'Sara Cill',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.',
+	},
+]
+
+export default Data
