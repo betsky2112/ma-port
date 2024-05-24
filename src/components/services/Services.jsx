8d9e5b4ed50378e8d6277c6bf8ce4d7de74747fb
betsky2112 <roberthussebastians@gmail.com>
2024-05-24 11:35:47 +0700

    Add qualification component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
 	return (
 		<section
 			className="services section"
-			id="sevices"
+			id="services"
 		>
 			<h2 className="section__title">Services</h2>
 			<span className="section__subtitle">What i offer</span>
